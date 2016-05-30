93212dd2bd36fe4822782c71c13b4880b58f2dc5
Chris Gibbons <cashiernumbersixplease@gmail.com>
2016-05-30 21:59:38 +0100

    feat: add click event listeners to gallery items

--- _source/scripts/main.js
+++ _source/scripts/main.js
@@ -8,18 +8,38 @@ window.FlickrGallery = (function(undefined)  {
     if(!document.getElementById(GalleryStage)){
       return;
     }
+
+    console.log(data);
     setupGallery(data);
+
+    // localstorage test
+
+    // setup event listeners
+    addListeners();
   }
 
   function setupGallery(data){
 
-    // setup event listeners
-
     // process data
+    window.GalleryReader.init(data);
 
-    // localstorage test
 
   }
+
+  function addListeners(){
+    var galleryEl = document.getElementsByClassName("c-gallery-tile");
+
+    for (var i = 0; i < galleryEl.length; i++) {
+      galleryEl[i].addEventListener("click", handleItemClick);
+    }
+
+  }
+
+  function handleItemClick(){
+  
+  }
+
+
 
   return {
     init: init
